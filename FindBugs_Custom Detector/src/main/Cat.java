ad0d35dfdb33033048a7871bc22b48a75ea30c17
Sunil L <sunilandroidnayak@gmail.com>
2017-10-31 16:39:14 +0530

    Added Custom Detectors and Custom Messages

--- FindBugs_Custom Detector/src/main/Cat.java
+++ FindBugs_Custom Detector/src/main/Cat.java
@@ -8,7 +8,15 @@ public class Cat
 {
     public static void main(String... args) throws IOException
     {
-        boolean ignore = ("Hello" == args[0]);
-        Files.lines(Paths.get(args[0])).forEach(System.out::println);
+//        boolean ignore = ("Hello" == args[0]);
+//        Files.lines(Paths.get(args[0])).forEach(System.out::println);
+        
+        String str_id = "100:200:Str";
+        str_test();
+    }
+    
+    public static void str_test()
+    {
+    	int x= 100;
     }
 }
--- FindBugs_Custom Detector/src/main/FilesLinesDetector.java
+++ FindBugs_Custom Detector/src/main/FilesLinesDetector.java
@@ -3,64 +3,132 @@ package main;
 
 import java.nio.file.Files;
 
-import org.apache.bcel.classfile.JavaClass;
+
+import org.apache.bcel.Constants;
+import org.apache.bcel.classfile.Constant;
+import org.apache.bcel.classfile.ConstantString;
 
 import edu.umd.cs.findbugs.BugInstance;
 import edu.umd.cs.findbugs.BugReporter;
 import edu.umd.cs.findbugs.BytecodeScanningDetector;
 import edu.umd.cs.findbugs.Priorities;
-import edu.umd.cs.findbugs.ba.ClassContext;
 import edu.umd.cs.findbugs.classfile.ClassDescriptor;
 import edu.umd.cs.findbugs.classfile.DescriptorFactory;
+import edu.umd.cs.findbugs.classfile.FieldDescriptor;
 import edu.umd.cs.findbugs.classfile.MethodDescriptor;
 
 public class FilesLinesDetector extends BytecodeScanningDetector
+
 {
+    String fieldName = "";
     private static final ClassDescriptor JAVA_NIO_FILES =
-        DescriptorFactory.createClassDescriptor(Files.class);
+            DescriptorFactory.createClassDescriptor(Files.class);
     final BugReporter bugReporter;
 
     public FilesLinesDetector(final BugReporter bugReporter)
     {
         this.bugReporter = bugReporter;
     }
-    
-//    @Override
-//    public void sawMethod()
-//    {
-//        final MethodDescriptor invokedMethod 
-//            = getMethodDescriptorOperand();
-//        final ClassDescriptor invokedObject 
-//            = getClassDescriptorOperand();
-//        if(invokedMethod != null && 
-//           "lines".equals(invokedMethod.getName()) && 
-//           JAVA_NIO_FILES.equals(invokedObject))
-//        {
-//            bugReporter.reportBug(
-//                 new BugInstance(this, 
-//                                 "FILES_LINES_CALLED", 
-//                                 HIGH_PRIORITY)
-//                     .addClassAndMethod(this)
-//                     .addSourceLine(this)
-//            );
-//        }
-//    }
-    
-    @Override
-	public void visitClassContext(ClassContext classContext) {
-		// TODO Auto-generated method stub
-	 JavaClass cls = classContext.getJavaClass();
-	 String clsname = cls.getClassName();
-	 
-	 if(clsname.charAt(0) != '1')
-	 {
-		 BugInstance bug = new BugInstance(this, 
-               "FILES_LINES_CALLED", 
-               HIGH_PRIORITY)
-   .addClassAndMethod(this)
-   .addSourceLine(this);
-		 bugReporter.reportBug(bug);
-	 }
- 
-	}
+
+    public void sawMethod()
+    {
+        final MethodDescriptor invokedMethod = getMethodDescriptorOperand();
+        // Cheking of method starting with s
+        if(invokedMethod != null && invokedMethod.getName().charAt(0) < 'a' || invokedMethod.getName().charAt(0) > 'z'){
+            BugInstance bug = new BugInstance(this,
+                    "METHOD_STARTING_WITH_SMALL",
+                    HIGH_PRIORITY)
+                    .addClassAndMethod(this)
+                    .addSourceLine(this);
+            bugReporter.reportBug(bug);
+        }
+
+        if(this.getMethodName().charAt(0) < 'a' || this.getMethodName().charAt(0) > 'z'){
+            BugInstance bug = new BugInstance(this,
+                    "METHOD_STARTING_WITH_SMALL",
+                    HIGH_PRIORITY)
+                    .addClassAndMethod(this)
+                    .addSourceLine(this);
+            bugReporter.reportBug(bug);
+        }
+    }
+
+    public void sawField()
+    {
+        final FieldDescriptor fieldDescriptor = getFieldDescriptor();
+        if(fieldDescriptor != null &&
+                (fieldDescriptor.getName().contains("id")))
+        {
+            bugReporter.reportBug(new BugInstance(
+                            this,
+                            "FILES_LINES_CALLED",
+                            HIGH_PRIORITY)
+                            .addClassAndMethod(this)
+                            .addSourceLine(this)
+            );
+        }
+
+
+        // if the field type is double throw a bug
+        if(fieldDescriptor != null &&
+                (fieldDescriptor.getSignature().equals("Double")))
+        {
+            bugReporter.reportBug(new BugInstance(
+                            this,
+                            "DOUBLE_DATA_TYPE_USED",
+                            HIGH_PRIORITY)
+                            .addClassAndMethod(this)
+                            .addSourceLine(this)
+            );
+        }
+        fieldName = this.getFieldName();
+        System.out.println("FN Value :"+this.getFieldName());
+    }
+
+    public void sawDouble(double seen)
+    {
+        bugReporter.reportBug(new BugInstance(
+                        this,
+                        "DOUBLE_DATA_TYPE_USED",
+                        HIGH_PRIORITY)
+                        .addClassAndMethod(this)
+                        .addSourceLine(this)
+        );
+
+
+    }
+
+    public void sawOpcode(int seen) {
+
+
+        if (seen != Constants.LDC
+                && seen != Constants.LDC_W) {
+            // If this bytecode is not a load constant,
+            // we're not interested.
+            return;
+        }
+
+        Constant operand = this.getConstantRefOperand();
+        if (!(operand instanceof ConstantString)) {
+            // If the constant being loaded is not a String,
+            // we're not interested.
+            return;
+        }
+
+        String value = ((ConstantString)operand).getBytes(this.getConstantPool());
+
+        if (!value.contains("/partition")) {
+            // The String literal being loaded is forbidden!
+            // Report Bug.
+            bugReporter.reportBug(new BugInstance(
+                            this,
+                            "ID_FIELD_WITHOUT_PARTITION",
+                            HIGH_PRIORITY)
+                            .addClassAndMethod(this)
+                            .addSourceLine(this)
+            );
+        }
+
+    }
+
 }
